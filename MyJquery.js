8834ae2ed83d30655ebe80d686b40b4d97b44007
Pawan <pawan.kashyap9@gmail.com>
2021-04-12 13:05:43 +0530

    This, Array

---
+++ MyJquery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    var mobs = ['Moto G',' One Pluse'];
+    $('#mobiles').html(mobs);
+
+    addElments(mobs);
+
+    $('#saveMobile').click(function(){
+        var _name=$('#mob').val();
+        mobs.push(' '+_name);
+        $('#mobiles').html(mobs);
+        $('#mob').val('');
+        addElments(mobs);
+    });
+
+    $('p').click(function(){
+        $(this).children('span').css({"background-color": "red", "color":"white"});
+
+    });
+
+});
+
+function addElments(mobList)
+{
+    $('#mobileList').empty();
+    $(mobList).each(function(){
+        $('<option>'+this+'</option>').appendTo('#mobileList');
+    });
+}
